cd26faf143462f3fc1ddbd37bc7fe1e671b09424
U-YE-AW15R3\Ye <wangyeee@gmail.com>
2019-04-02 15:17:29 +1300

    Update change_text_size.py to a KiCAD plugin.

--- change_text_size/change_text_size.py
+++ change_text_size/change_text_size.py
@@ -1,22 +1,29 @@
 import pcbnew
 
-board = pcbnew.GetBoard()
+class SmallTextSize(pcbnew.ActionPlugin):
 
-newThickness = 100000
-newWidth = 400000
-newHeight = 400000
+    def defaults(self):
+        self.name = 'SmallTextSize'
+        self.category = 'Modify PCB'
+        self.description = 'Make texts smaller on PCB'
 
-for module in board.GetModules():
-    val = module.Value()
-    val.SetThickness(newThickness)
-    val.SetTextWidth(newWidth)
-    val.SetTextHeight(newHeight)
-    ref = module.Reference()
-    ref.SetThickness(newThickness)
-    ref.SetTextWidth(newWidth)
-    ref.SetTextHeight(newHeight)
-    for otr in module.GraphicalItemsList():
-        if isinstance(otr, pcbnew.TEXTE_MODULE):
-            otr.SetVisible(False)
+    def Run(self):
+        board = pcbnew.GetBoard()
+        newThickness = 100000
+        newWidth = 400000
+        newHeight = 400000
+        for module in board.GetModules():
+            val = module.Value()
+            val.SetThickness(newThickness)
+            val.SetTextWidth(newWidth)
+            val.SetTextHeight(newHeight)
+            ref = module.Reference()
+            ref.SetThickness(newThickness)
+            ref.SetTextWidth(newWidth)
+            ref.SetTextHeight(newHeight)
+            for otr in module.GraphicalItemsList():
+                if isinstance(otr, pcbnew.TEXTE_MODULE):
+                    otr.SetVisible(False)
+        pcbnew.Refresh()
 
-pcbnew.Refresh()
+SmallTextSize().register()
